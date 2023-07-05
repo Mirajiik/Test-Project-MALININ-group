4ead7cdac38e399d43c57a83fa750864e4f36501
Miraj <mirajiik@yandex.ru>
2023-07-06 02:56:12 +0500

    Create Async procedure

--- MainForm.dfm
+++ MainForm.dfm
@@ -2,8 +2,8 @@ object Form1: TForm1
   Left = 0
   Top = 0
   Caption = 'SortNumSequence'
-  ClientHeight = 570
-  ClientWidth = 762
+  ClientHeight = 579
+  ClientWidth = 521
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
   Font.Color = clWindowText
@@ -14,12 +14,13 @@ object Form1: TForm1
   TextHeight = 15
   object PanelToolBar: TPanel
     Left = 0
-    Top = 450
-    Width = 762
+    Top = 459
+    Width = 521
     Height = 120
     Align = alBottom
-    Caption = 'PanelToolBar'
     TabOrder = 0
+    ExplicitTop = 457
+    ExplicitWidth = 715
     object PanelGenerateSeq: TPanel
       Left = 1
       Top = 1
@@ -73,36 +74,89 @@ object Form1: TForm1
         OnClick = BtnGenerateSeqClick
       end
     end
+    object GBToolBarBubbleSort: TGroupBox
+      Left = 360
+      Top = 1
+      Width = 160
+      Height = 118
+      Align = alRight
+      Caption = #1057#1086#1088#1090#1080#1088#1086#1074#1082#1072' "'#1055#1091#1079#1099#1088#1100#1082#1086#1084'"'
+      Font.Charset = DEFAULT_CHARSET
+      Font.Color = clWindowText
+      Font.Height = -13
+      Font.Name = 'Segoe UI'
+      Font.Style = []
+      ParentFont = False
+      TabOrder = 1
+      ExplicitLeft = 454
+    end
+    object GBToolBarQuickSort: TGroupBox
+      Left = 200
+      Top = 1
+      Width = 160
+      Height = 118
+      Align = alRight
+      Caption = '"'#1041#1099#1089#1090#1088#1072#1103'" '#1089#1086#1088#1090#1080#1088#1086#1074#1082#1072
+      Font.Charset = DEFAULT_CHARSET
+      Font.Color = clWindowText
+      Font.Height = -13
+      Font.Name = 'Segoe UI'
+      Font.Style = []
+      ParentFont = False
+      TabOrder = 2
+      ExplicitLeft = 294
+      object LabelTimeAndProgress: TLabel
+        Left = 2
+        Top = 91
+        Width = 156
+        Height = 25
+        Align = alBottom
+        Caption = #1042#1088#1077#1084#1103':'
+        Font.Charset = DEFAULT_CHARSET
+        Font.Color = clWindowText
+        Font.Height = -19
+        Font.Name = 'Segoe UI'
+        Font.Style = []
+        ParentFont = False
+        ExplicitLeft = 24
+        ExplicitTop = 144
+        ExplicitWidth = 59
+      end
+      object BtnQuickSortBothSeq: TButton
+        Left = 3
+        Top = 45
+        Width = 140
+        Height = 40
+        Caption = #1057#1086#1088#1090#1080#1088#1086#1074#1082#1072
+        Font.Charset = DEFAULT_CHARSET
+        Font.Color = clWindowText
+        Font.Height = -19
+        Font.Name = 'Segoe UI'
+        Font.Style = []
+        ParentFont = False
+        TabOrder = 0
+        WordWrap = True
+        OnClick = BtnQuickSortBothSeqClick
+      end
+    end
   end
-  object PanelLBs: TPanel
+  object LBSeq1: TListBox
     Left = 0
     Top = 0
-    Width = 762
-    Height = 450
+    Width = 521
+    Height = 459
     Align = alClient
+    Font.Charset = DEFAULT_CHARSET
+    Font.Color = clWindowText
+    Font.Height = -16
+    Font.Name = 'Segoe UI'
+    Font.Style = []
+    ItemHeight = 21
+    ParentFont = False
     TabOrder = 1
-    ExplicitLeft = 408
-    ExplicitTop = 264
-    ExplicitWidth = 185
-    ExplicitHeight = 41
-    object LBSeq1: TListBox
-      Left = 1
-      Top = 1
-      Width = 360
-      Height = 448
-      Align = alLeft
-      ItemHeight = 15
-      TabOrder = 0
-    end
-    object LBSeq2: TListBox
-      Left = 401
-      Top = 1
-      Width = 360
-      Height = 448
-      Align = alRight
-      ItemHeight = 15
-      TabOrder = 1
-      ExplicitLeft = 640
-    end
+    ExplicitLeft = 81
+    ExplicitTop = 25
+    ExplicitWidth = 240
+    ExplicitHeight = 389
   end
 end
--- MainForm.pas
+++ MainForm.pas
@@ -3,8 +3,8 @@ unit MainForm;
 interface
 
 uses
-  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls, Vcl.StdCtrls;
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, System.Diagnostics,
+   Vcl.Graphics, Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls, Vcl.StdCtrls;
 
 type
   TForm1 = class(TForm)
@@ -13,24 +13,31 @@ type
     EditLengthSeq: TEdit;
     LabelLengthSeq: TLabel;
     BtnGenerateSeq: TButton;
-    PanelLBs: TPanel;
+    GBToolBarBubbleSort: TGroupBox;
+    GBToolBarQuickSort: TGroupBox;
+    BtnQuickSortBothSeq: TButton;
+    LabelTimeAndProgress: TLabel;
     LBSeq1: TListBox;
-    LBSeq2: TListBox;
     procedure EditLengthSeqChange(Sender: TObject);
     procedure FormCreate(Sender: TObject);
     procedure BtnGenerateSeqClick(Sender: TObject);
+    procedure BtnQuickSortBothSeqClick(Sender: TObject);
   private
     FLengthSeq: Integer;
+    FTimeRun: String;
     procedure SetLengthSeq(const Value: Integer);
+    procedure SetTimeRun(const Value: String);
+    procedure qSort(var A: TArray<Double>; min, max: Integer);
     procedure UpdateListBoxs;
   public
     property LengthSeq: Integer read FLengthSeq write SetLengthSeq;
+    property TimeRun: String read FTimeRun write SetTimeRun;
   end;
 
 var
   Form1: TForm1;
-  NumSeq1 : array of Double;
-  NumSeq2 : array of Double;
+  NumSeq1 : TArray<Double>;
+  //NumSeq2 : TArray<Double>;
   TempList: TStrings;
 
 implementation
@@ -41,33 +48,69 @@ procedure TForm1.FormCreate(Sender: TObject);
 begin
   FLengthSeq := -1;
   LengthSeq := 0;
-  TempList := TStringList.Create;
 end;
 
 procedure TForm1.BtnGenerateSeqClick(Sender: TObject);
 begin
   NumSeq1 := nil;
-  NumSeq2 := nil;
+  //NumSeq2 := nil;
   SetLength(NumSeq1, LengthSeq);
-  SetLength(NumSeq2, LengthSeq);
+  //SetLength(NumSeq2, LengthSeq);
   var Temp: Double;
   for var I := 0 to LengthSeq-1 do
   begin
     Temp := Random*10000;
     NumSeq1[I] := Temp;
-    NumSeq2[I] := Temp;
-    ShowMessage(FloatToStr(Temp));
+    //NumSeq2[I] := Temp;
+    //ShowMessage(FloatToStr(Temp));
   end;
   UpdateListBoxs;
 end;
 
 procedure TForm1.UpdateListBoxs;
+var
+  I : Integer;
 begin
   LBSeq1.Clear;
-  LBSeq2.Clear;
-  //TempList.Assign(NumSeq1);
-  //LBSeq1.Items.Assign(NumSeq1);
-  //Seq2.Items.Assign(Items);
+  //LBSeq2.Clear;
+  for I := 0 to LengthSeq-1 do
+  begin
+    LBSeq1.Items.Add(FloatToStr(NumSeq1[I]));
+    //Seq2.Items.Add(FloatToStr(NumSeq2[I]));
+  end;
+end;
+
+procedure TForm1.qSort(var A: TArray<Double>; min, max: Integer);
+var
+  i, j: Integer;
+  supp, tmp: Double;
+begin
+supp:=A[max-((max-min) div 2)];
+i:=min; j:=max;
+while i<j do
+  begin
+    while A[i]<supp do i:=i+1;
+    while A[j]>supp do j:=j-1;
+    if i<=j then
+      begin
+        tmp:=A[i]; A[i]:=A[j]; A[j]:=tmp;
+        UpdateListBoxs;
+        i:=i+1; j:=j-1;
+      end;
+  end;
+if min<j then qSort(A, min, j);
+if i<max then qSort(A, i, max);
+end;
+
+procedure TForm1.BtnQuickSortBothSeqClick(Sender: TObject);
+var
+  Stopwatch: TStopwatch;
+begin
+  Stopwatch := TStopwatch.Create;
+  Stopwatch.Start;
+  qSort(NumSeq1, 0, High(NumSeq1));
+  Stopwatch.Stop;
+  TimeRun := Stopwatch.ElapsedMilliseconds.ToString;
 end;
 
 procedure TForm1.EditLengthSeqChange(Sender: TObject);
@@ -84,4 +127,12 @@ begin
   end;
 end;
 
+procedure TForm1.SetTimeRun(const Value: string);
+begin
+  if FTimeRun <> Value then
+  begin
+    FTimeRun := Value;
+    LabelTimeAndProgress.Caption := 'Время: ' + FTimeRun + 'ms';
+  end;
+end;
 end.
